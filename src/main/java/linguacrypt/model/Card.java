faf6e5ef021fca51557c0aa117292cab2543c45f
Tom Bloch <61981635+BTom-GH@users.noreply.github.com>
2025-01-06 16:21:14 +0100

    [+] feat : ajout des classes restantes du modèle Reste : adapter Game au modèle définit dans le diagramme UML, et adapter la sauvegarde en conséquence

--- src/main/java/linguacrypt/model/Card.java
+++ src/main/java/linguacrypt/model/Card.java
@@ -1,5 +1,40 @@
 package linguacrypt.model;
 
-public class Card {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Represents a card in the game with a name and a color.
+ */
+public class Card {
+    @JsonProperty("cardName")
+    private String cardName;
+    
+    @JsonProperty("cardColor")
+    private Color cardColor;
+
+    public Card(String cardName) {
+        this.cardName = cardName;
+        this.cardColor = Color.WHITE; // Default color, neutral card
+    }
+
+    @JsonCreator
+    public Card(
+        @JsonProperty("cardName") String cardName,
+        @JsonProperty("cardColor") Color cardColor) {
+        this.cardName = cardName;
+        this.cardColor = cardColor;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public Color getCardColor() {
+        return cardColor;
+    }
+
+    public void setCardColor(Color cardColor) {
+        this.cardColor = cardColor;
+    }
 }

0d2bb6714860308cfbbf3d5b2f9ae69d45716355
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2025-01-07 17:54:21 +0100

    [+] preparing for Image cards with URLs

--- src/main/java/linguacrypt/model/Card.java
+++ src/main/java/linguacrypt/model/Card.java
@@ -1,8 +1,14 @@
 package linguacrypt.model;
 
+import java.io.File;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 /**
  * Represents a card in the game with a name and a color.
  */
@@ -16,18 +22,33 @@ public class Card {
     @JsonProperty("found")
     private boolean found;
 
+    @JsonProperty("cardUrl")
+    private String cardUrl;
+
     public Card(String cardName) {
         this.cardName = cardName;
         this.cardColor = Color.WHITE;// Default color, neutral card
         this.found = false;
+        this.cardUrl = null;
     }
 
+    public Card(String cardName,String cardUrl) {
+        this.cardName = cardName;
+        this.cardColor = Color.WHITE;// Default color, neutral card
+        this.found = false;
+        this.cardUrl = cardUrl;
+    }
+    
     @JsonCreator
     public Card(
         @JsonProperty("cardName") String cardName,
-        @JsonProperty("cardColor") Color cardColor) {
+        @JsonProperty("cardColor") Color cardColor,
+        @JsonProperty("found") boolean found,
+        @JsonProperty("cardUrl") String cardUrl){
         this.cardName = cardName;
         this.cardColor = cardColor;
+        this.found = found;
+        this.cardUrl = cardUrl;
     }
 
     public String getCardName() {
@@ -48,5 +69,18 @@ public class Card {
 
     public void reveal() {
         this.found = true;
+    }
+
+    public String getCardUrl() {
+        return cardUrl;
+    }
+
+    public Node getCardView() {
+        if (cardUrl != null) {
+            File file = new File(cardUrl);
+            Image img = new Image(file.toURI().toString());
+            return new ImageView(img);
+        }
+        return new Label(cardName);
     }
 }
